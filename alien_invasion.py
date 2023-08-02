d72c5586189155896bf8096ce238e9d334f7bc76
Mike <mtefauthor@gmail.com>
2023-08-02 18:20:09 +0100

    Add alien fleet.

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Overall class to manage game assets & behaviour."""
@@ -22,6 +23,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
     
 
     def run_game(self):
@@ -32,6 +36,23 @@ class AlienInvasion:
             self._update_bullets()
             self._update_screen()
             self.clock.tick(60)
+
+    def _create_fleet(self):
+        """Create the fleet of aliens."""
+        # Create an alien and keep adding aliens until there's no room left.
+        # Spacing between aliens is one alien width.
+        alien = Alien(self)
+        alien_width = alien.rect.width
+
+        current_x = alien_width
+        while current_x < (self.settings.screen_width - 2 * alien_width):
+            new_alien = Alien(self)
+            new_alien.x = current_x
+            new_alien.rect.x = current_x
+            self.aliens.add(new_alien)
+            current_x += 2 * alien_width
+
+        #self.aliens.add(alien)
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
@@ -98,6 +119,7 @@ class AlienInvasion:
             bullet.draw_bullet()
         
         self.ship.blitme()
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
 
